5b7c8b57c8c202856e54659daf0fab6eced349f5
Philippe Simons <simons.philippe@gmail.com>
2022-02-16 15:26:22 +0100

    WIP

---
+++ libretro/libretro.cpp
@@ -0,0 +1,8 @@
+#include "libretro.h"
+
+static retro_log_printf_t log_cb;
+static retro_video_refresh_t video_cb;
+static retro_audio_sample_batch_t audio_batch_cb;
+static retro_environment_t environ_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
