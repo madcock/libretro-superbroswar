19860a4f68dfa352f675766ba6de3e155246040e
Philippe Simons <simons.philippe@gmail.com>
2022-02-26 10:47:04 +0100

    add libretro gitlab config

---
+++ jni/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+CORE_DIR := $(LOCAL_PATH)/..
+
+DEBUG                    := 0
+FRONTEND_SUPPORTS_RGB565 := 1
+NEED_BPP                 := 16
+NEED_STEREO_SOUND        := 1
+HAVE_CHD                 := 0
+IS_X86                   := 0
+FLAGS                    :=
+
+ifeq ($(TARGET_ARCH),x86)
+  IS_X86 := 1
+endif
+
+include $(CORE_DIR)/Makefile.common
+
+COREFLAGS := $(FLAGS) -funroll-loops $(INCFLAGS) -D__LIBRETRO__ $(CORE_DEFINE)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := retro
+LOCAL_SRC_FILES    := $(SOURCES_C) $(SOURCES_CXX)
+LOCAL_CFLAGS       := $(COREFLAGS)
+LOCAL_CXXFLAGS     := $(CXXFLAGS) $(COREFLAGS)
+LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
+include $(BUILD_SHARED_LIBRARY)
